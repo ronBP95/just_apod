0823dc209ddbd8fcc86ea6ce530d26622e46a3aa
ronbp <ronmagpantay9513@gmail.com>
2022-02-17 00:00:29 -0800

    express installed, basic routes built out, nodemon to be installed

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen (PORT, () => console.log(`Server started on Port: ${PORT}`))

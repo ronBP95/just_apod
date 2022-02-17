00f8fbd7d6cae56a1f35f4ddbd4f21579a2e1ebf
ronbp <ronmagpantay9513@gmail.com>
2022-02-17 01:05:20 -0800

    starting to build out axios fetches

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Express, { urlencoded } from "express"
 import path from "path"
 import { fileURLToPath } from "url";
+import axios from "axios"
 
 const app = Express();
 const PORT = process.env.PORT || 3000;
@@ -13,5 +14,19 @@ app.use(Express.urlencoded({extended: false}));
 
 // Set static folder
 app.use(Express.static(path.join(__dirname, 'public')));
+
+// ROUTES
+axios.get ('/api/wow')
+    .then(function (response) {
+        //handle success
+        console.log("success")
+    })
+    .catch(function (error) {
+        console.log("failure")
+    })
+    .then(function() {
+        console.log("this is where it executes")
+    })
+//
 
 app.listen (PORT, () => console.log(`Server started on Port: ${PORT}`))

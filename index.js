2a433fa637b1616f39c0e136bcbe322dbdcc0fee
ronbp <ronmagpantay9513@gmail.com>
2022-02-17 00:12:30 -0800

    express boilerplate built out

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import express from "express"
+import Express, { urlencoded } from "express"
+import path from "path"
+import { fileURLToPath } from "url";
 
-const app = express();
+const app = Express();
 const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+// Body Parser Middleware
+app.use(Express.json());
+app.use(Express.urlencoded({extended: false}));
+
+// Set static folder
+app.use(Express.static(path.join(__dirname, 'public')));
 
 app.listen (PORT, () => console.log(`Server started on Port: ${PORT}`))

9c3d930363325ca0e239f9d26516ec6497cb7a1d
ronbp <ronmagpantay9513@gmail.com>
2022-02-18 00:33:13 -0800

    commented out static folder since we will be using dynamic assets

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Express, { urlencoded } from "express"
 import path from "path"
 import { fileURLToPath } from "url";
 import axios from "axios"
+import fs from "fs"
 
 const app = Express();
 const PORT = process.env.PORT || 3000;
@@ -12,21 +13,19 @@ const __dirname = path.dirname(__filename);
 app.use(Express.json());
 app.use(Express.urlencoded({extended: false}));
 
-// Set static folder
-app.use(Express.static(path.join(__dirname, 'public')));
+// Set static folder (disabled as dynamic assets are being used)
+// app.use(Express.static(path.join(__dirname, 'public')));
+
+let myCss  ={
+    style : fs.readFileSync('./public/css/style.css', 'utf8')
+};
 
 // ROUTES
-axios.get ('/api/wow')
-    .then(function (response) {
-        //handle success
-        console.log("success")
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+        title: 'just_APOD',
+        myCss: myCss
     })
-    .catch(function (error) {
-        console.log("failure")
-    })
-    .then(function() {
-        console.log("this is where it executes")
-    })
-//
+})
 
 app.listen (PORT, () => console.log(`Server started on Port: ${PORT}`))
